89bf90394ab526c31229f685c4913d2a5e75d153
Phhere <P.Rehs@gmx.net>
2013-09-30 16:41:38 +0200

    Inital Version

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="bootstrap/assets/ico/favicon.png">
+
+  <title>Random Steam Game</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="style/jumbotron-narrow.css" rel="stylesheet">
+  <link href="style/page.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+      <![endif]-->
+    </head>
+
+    <body>
+
+      <div class="container">
+        <div class="header">
+          <ul class="nav nav-pills pull-right">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <h3 class="text-muted">Random Steam Game</h3>
+        </div>
+
+
+        <div class="row">
+           <div class="col-lg-12">
+             <form method="post">
+               <div class="input-group">
+                <input type="text" name="username" value="<?php echo htmlentities(@$_REQUEST['username']);?>" class="form-control">
+                <span class="input-group-btn">
+                  <button class="btn btn-default" type="button">Go!</button>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+        <br/>
+        <?php
+        if(isset($_REQUEST['username']) && !empty(trim($_REQUEST['username']))){
+          include("query.php");
+        }
+        else {
+        ?>
+        <div class="jumbotron">
+          <h1>Random Steam Game</h1>
+          <p class="lead">Enter your Steam-Name or SteamID to find a game to play. You can enter multiple names (seperated by comma) to find a game you could play together with your friends</p>
+        </div>
+        <div class="row marketing">
+          <div class="col-lg-6">
+            <h4>Subheading</h4>
+            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+            <h4>Subheading</h4>
+            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+            <h4>Subheading</h4>
+            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+          </div>
+
+          <div class="col-lg-6">
+            <h4>Subheading</h4>
+            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+            <h4>Subheading</h4>
+            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+            <h4>Subheading</h4>
+            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+          </div>
+        </div>
+        <?php
+      }
+      ?>
+
+        <div class="footer">
+          <p>&copy; Company 2013</p>
+        </div>
+
+      </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+  </html>
---
+++ query.php
@@ -0,0 +1,93 @@
+<?php
+include 'api/SteamAPI.php';
+include 'api/SteamAPIDriver.php';
+
+$steamdriver = new Neoseeker\SteamAPI\SteamAPIDriver;
+$api = new Neoseeker\SteamAPI\SteamAPI($steamdriver);
+if(stristr($_REQUEST['username'], ",")){
+    $infos = array();
+    $accounts = explode(",",$_REQUEST['username']);
+    $usable_games = array();
+    foreach ($accounts as $name) {
+        $name = trim($name);
+        $infos[$name] = array();
+        $api->load($name);
+        $infos[$name]['games'] = array();
+        if($api->information == false){
+            $infos[$name]['status'] = "Can't query account data. Maybe does the account not exist";
+        }
+        elseif(isset($api->information->privacyMessage)){
+             $infos[$name]['status'] = $api->information->privacyMessage;
+        }
+        elseif($api->information->privacyState != "public"){
+            $infos[$name]['status'] = "Account is not public";
+        }
+        else{
+            $games = $api->get_games();
+            if(count($games)){
+                $infos[$name]['games'] = $games;
+                $usable_games = $games;
+            }
+            else{
+                $infos[$name]['status'] = "No Games available";
+            }
+        }
+    }
+    function get_games($account){
+        return array_keys($account['games']);
+    }
+    $games = array_values(array_filter(array_map("get_games", $infos)));
+    $keys = call_user_func_array("array_intersect",$games);
+    foreach($infos as $account => $data){
+        if(isset($data['status'])){
+            echo "<div class='alert alert-warning'>Account ".$account.": ".$data['status']."</div>";
+        }
+    }
+    if(count($keys) == 0){
+        echo "<div class='alert alert-danger'>Can't find game you all own</div>";
+    }
+    else{
+        $launch = $usable_games[$keys[array_rand($keys)]];
+        echo '<div class="jumbotron">
+                      <h1>'.$launch->name.'</h1>
+                      <a href="steam://run/'.$launch->appID.'" class="btn btn-success">Launch</a>
+                    </div>';
+        echo "<div class='well'>Game: ".count($keys)."</div>";
+        foreach($keys as $gameID){
+            $game = $usable_games[$gameID];
+            echo "<div class='row well game'><img src='".$game->logo."' width='200' class='img-thumbnail pull-left' /><a href='steam://run/".$game->appID."' class='btn btn-success pull-right'>Launch</a><h4>".$game->name."</h4></div>";
+        }
+    }
+}
+else{
+    $api->load($_REQUEST['username']);
+    if($api->information == false){
+        echo "<div class='alert alert-danger'>Can't query account data. Maybe does the account not exist</div>";
+    }
+    elseif(isset($api->information->privacyMessage)){
+        echo "<div class='alert alert-danger'>".$api->information->privacyMessage."</div>";
+    }
+    elseif($api->information->privacyState != "public"){
+        echo "<div class='alert alert-danger'>Account is not public</div>";
+    }
+    else{
+        $games = $api->get_games();
+        if(count($games)){
+            $launch = $games[array_rand($games)];
+            echo '<div class="jumbotron">
+                      <h1>'.$launch->name.'</h1>
+                      <a href="steam://run/'.$launch->appID.'" class="btn btn-success">Launch</a>
+                    </div>';
+            echo "<div class='well'>Game: ".count($games)."</div>";
+            foreach($games as $game){
+                if(isset($game->hoursOnRecord)) $stats = "Played ".$game->hoursOnRecord." hours";
+                else $stats = "not played yet";
+                echo "<div class='row well game'><img src='".$game->logo."' width='200' class='img-thumbnail pull-left' /><a href='steam://run/".$game->appID."' class='btn btn-success pull-right'>Launch</a><h4>".$game->name."</h4><b>".$stats."</b></div>";
+            }
+        }
+        else{
+            echo "No Games available";
+        }
+    }
+}
+?>
